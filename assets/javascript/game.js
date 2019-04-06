e5ef801c5a98f690df7b2fd01b3591e715f7fb66
patricia-dalinis <47152085+patricia-dalinis@users.noreply.github.com>
2019-04-06 00:56:49 -0500

    Final Push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var random = Math.floor(Math.random() * ((120 - 19) + 1)) + 19;
     console.log("Random Number", random);
     $("#randomNumber").text(random);
@@ -7,7 +7,7 @@ $(document).ready(function() {
     var gem2 = Math.floor(Math.random() * ((12 - 1) + 1)) + 1;
     var gem3 = Math.floor(Math.random() * ((12 - 1) + 1)) + 1;
     var gem4 = Math.floor(Math.random() * ((12 - 1) + 1)) + 1;
-    console.log(gem1 , gem2 , gem3 , gem4);
+    console.log(gem1, gem2, gem3, gem4);
 
     var userGuess = 0;
     var wins = 0;
@@ -18,41 +18,45 @@ $(document).ready(function() {
 
     function reset() {
         random = Math.floor(Math.random() * ((120 - 19) + 1)) + 19;
+        console.log(random);
         $("#randomNumber").text(random);
         gem1 = Math.floor(Math.random() * ((12 - 1) + 1)) + 1;
         gem2 = Math.floor(Math.random() * ((12 - 1) + 1)) + 1;
         gem3 = Math.floor(Math.random() * ((12 - 1) + 1)) + 1;
         gem4 = Math.floor(Math.random() * ((12 - 1) + 1)) + 1;
+        console.log(gem1, gem2, gem3, gem4);
         userGuess = 0;
         $("#finalGuess").text(userGuess);
     }
 
     function win() {
-        $("#winOrLose").text("You Won!");
+        $("#winOrLose").text("You Won! Keep it up!");
         wins++;
         $("#numberOfWins").text(wins);
+        console.log("You Won!");
         reset();
     }
 
     function lose() {
-        $("#winOrLose").text("You Lost!");
+        $("#winOrLose").text("You Lost! Try again!");
         losses++;
         $("#numberOfLosses").text(losses);
+        console.log("You Lost!");
         reset();
     }
 
-    $("#one").on('click', function() {
+    $("#one").on('click', function () {
         userGuess = userGuess + gem1;
         console.log("Guess: " + userGuess);
         $("#finalGuess").text(userGuess);
         if (userGuess == random) {
             win();
-        } else if (userGuess > random){
+        } else if (userGuess > random) {
             lose();
         }
     });
 
-    $("#two").on('click', function() {
+    $("#two").on('click', function () {
         userGuess = userGuess + gem2;
         console.log("Guess: " + userGuess);
         $("#finalGuess").text(userGuess);
@@ -63,7 +67,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#three").on('click', function() {
+    $("#three").on('click', function () {
         userGuess = userGuess + gem3;
         console.log("Guess: " + userGuess);
         $("#finalGuess").text(userGuess);
@@ -74,7 +78,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#four").on('click', function() {
+    $("#four").on('click', function () {
         userGuess = userGuess + gem4;
         console.log("Guess: " + userGuess);
         $("#finalGuess").text(userGuess);
